65fa7a28c7fab9938fa84e49be2f137d335867e7
houssemeddineabbou <houssemeddineabbou@yahoo.com>
2023-10-23 22:46:30 +0100

    Creation of main menu

--- Projet-Python-HoussemEddineAbbou.py
+++ Projet-Python-HoussemEddineAbbou.py
@@ -1,8 +1,21 @@
+import cowsay
+
 #---------------------------------------------------------------------------------#
 #                                   MAIN                                          # 
 #---------------------------------------------------------------------------------#
 def main():
     print("Welcome to Houssem's project")
+    while True:
+        cowsay.cow("1. Register \n"
+                    "2. Authenticate")
+
+        choice = input("Choose option: ")
+        if choice == "1":
+            cowsay.cow("Option 1")
+        elif choice == "2":
+            cowsay.cow("Option 2")
+        else:
+            cowsay.cow("Invalid choice")
 
 if __name__ == "__main__":
     main()

06209fbcd26544d4d7af3d7d5c004e6f97878350
houssemeddineabbou <houssemeddineabbou@yahoo.com>
2023-10-25 22:13:11 +0100

    Authentication&Menu

--- Projet-Python-HoussemEddineAbbou.py
+++ Projet-Python-HoussemEddineAbbou.py
@@ -1,6 +1,8 @@
 import cowsay
 import re
 import stdiomask
+import hashlib
+import bcrypt
 
 
 #---------------------------------------------------------------------------------#
@@ -37,13 +39,78 @@ def saveCredentials(email, password):
         file.write(f"{email}|{password}\n")
 
 #---------------------------------------------------------------------------------#
+#                                  2- Authentification                            # 
+#---------------------------------------------------------------------------------#
+def authenticateUser(email, password):
+    try:
+        with open("Enregistrement.txt", "r") as f:
+            for line in f:
+                emailFile, passwordFile = line.strip().split("|")
+                if email == emailFile and password == passwordFile:
+                    return True
+    except FileNotFoundError:
+        print("File not found")
+    return False
+
+#---------------------------------------------------------------------------------#
+#                 Le menu, une fois authentifié,est comme suit                    # 
+#---------------------------------------------------------------------------------#
+def showMenuAuthenticated():
+    while True:
+        cowsay.cow("A- Give a word to hash (in invisible mode)\n"
+                   "B- Encryption (RSA)\n"
+                   "C- Certificate (RSA)\n"
+                   "D- Quit")
+        choice = input("Enter your choice: ")
+        if choice == "A":
+            invisibileMode()
+        elif choice == "B":
+            print("Your choice is B")
+        elif choice == "C":
+            print("Your choice is C")
+        elif choice == "D":
+            cowsay.cow("Merci pour votre attention moooo KEKW")
+            exit(0)
+        else:
+            cowsay.cow("Invalid choice")
+
+#---------------------------------------------------------------------------------#
+#                 A- Donnez un mot à haché (en mode invisible)                    # 
+#---------------------------------------------------------------------------------#
+def invisibileMode():
+    while True:
+        cowsay.cow("a- Hash the word with SHA-256\n"
+                   "b- Hash the word by generating a salt (bcrypt)\n"
+                   "c- Dictionary attack on the inserted word\n"
+                   "d- Return to the main menu")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "a":
+            textSHA256 = input("Enter the text to hash using SHA-256: ")
+            sha256Hash = hashlib.sha256(textSHA256.encode()).hexdigest()
+            print("SHA-256 Hash:", sha256Hash)
+        elif choice == "b":
+            textBcrypt = input("Enter the text to hash using bcrypt with salt: ")
+            salt = bcrypt.gensalt()
+            bcryptHash = bcrypt.hashpw(textBcrypt.encode(), salt)
+            print("Salted Hash (bcrypt):", bcryptHash)
+        elif choice == "c":
+            word = input("Type a word to perform a dictionary attack: ")
+            print("Dictionary attack")
+        elif choice == "d":
+            return
+        else:
+            print("Invalid choice")
+
+#---------------------------------------------------------------------------------#
 #                                   MAIN                                          # 
 #---------------------------------------------------------------------------------#
 def main():
     print("Welcome to Houssem's project")
     while True:
         cowsay.cow("1. Register \n"
-                    "2. Authenticate")
+                   "2. Authenticate")
 
         choice = input("Choose option: ")
         if choice == "1":
@@ -52,7 +119,13 @@ def main():
             saveCredentials(email, password)
             cowsay.cow("Registration done")
         elif choice == "2":
-            cowsay.cow("Option 2 : working on it")
+            email = enterEmail()
+            password = enterPassword()
+            if authenticateUser(email, password):
+                print("Authentication done")
+                showMenuAuthenticated()
+            else:
+                cowsay.cow("Authentication failed")
         else:
             cowsay.cow("Invalid choice")
 
